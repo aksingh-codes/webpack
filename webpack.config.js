0037a4dcf7122487bc698b67509371e9962d176b
aksingh <dev.ak.singh@outlook.com>
2022-06-03 12:38:53 +0530

    Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
   mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -24,36 +31,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
         loader: "file-loader",
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
       },
     ],
   },
